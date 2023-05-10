b464f8e8d77e12979a379c3b4e923cd5879f0e33
zayman2045 <xavierorlandogriffith@gmail.com>
2023-05-09 20:43:10 -0400

    Adds HTTP GET request to imdb top 100 movies based on user rating

---
+++ imdb/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // HTTP GET request
+    let response = reqwest::blocking::get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100")
+    .unwrap()
+    .text()
+    .unwrap();
+
+    println!("{response}");
+}
